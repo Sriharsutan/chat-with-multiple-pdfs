877f2bbf45f2df44ffcb6a649d73f307334f480e
sriharsutan <sriharsutan@gmail.com>
2025-06-11 19:06:11 +0530

    added vetor store and conversation chain

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+#from langchain.embeddings import HuggingFaceInstructEmbeddings
+#from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+#from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import conversational_retrieval
+from langchain.llms import openai
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -21,6 +30,20 @@ def get_chunks(raw_text):
     chunks = text_splitter.split_text(raw_text)
     return chunks
 
+def get_vectorstore(chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="hkunlp/instructor-xl")
+    vector_store = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    return vector_store
+
+def get_conversation(vector_store):
+    llm = openai()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = conversational_retrieval(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
 def main():
     load_dotenv()
@@ -28,6 +51,9 @@ def main():
 
     st.header("Chat with Multiple PDFs :books:")
     st.text_input("Ask a question about your documents: ")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     with st.sidebar:
         st.subheader("Your Documents")
@@ -41,9 +67,13 @@ def main():
 
                 #get the text chunks 
                 chunks = get_chunks(raw_text)
-                st.write(chunks)
-                break
+                #st.write(chunks)
+                
                 #create a vector store
+                vector_store = get_vectorstore(chunks)
+
+                #creating a conversation chain
+                conversation = get_conversation(vector_store)
 
 if __name__ == '__main__':
     main()
