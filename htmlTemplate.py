f16d721e64e7a21af8f2165c7d6873051c68ad00
sriharsutan <sriharsutan@gmail.com>
2025-06-14 13:12:48 +0530

    updated the template

--- htmlTemplate.py
+++ htmlTemplate.py
@@ -1,44 +1,81 @@
 css = '''
 <style>
 .chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+    padding: 1.5rem;
+    border-radius: 0.75rem;
+    margin-bottom: 1rem;
+    display: flex;
+    align-items: flex-start;
+    box-shadow: 0 4px 12px rgba(0,0,0,0.1);
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #2b313e;
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #475063;
 }
 .chat-message .avatar {
-  width: 20%;
+    width: 15%;
+    text-align: center;
 }
 .chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
+    max-width: 64px;
+    max-height: 64px;
+    border-radius: 50%;
+    object-fit: cover;
 }
 .chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
+    width: 85%;
+    padding: 0 1rem;
+    color: #fff;
 }
+.chat-message .content {
+    font-size: 1rem;
+    margin-bottom: 0.3rem;
+}
+.chat-message .timestamp {
+    font-size: 0.75rem;
+    color: #d0d0d0;
+    text-align: right;
+}
+</style>
 '''
 
-bot_template = '''
-<div class="chat-message bot">
+
+import base64
+import datetime
+
+
+def img_to_base64(path):
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+def get_timestamp():
+    return datetime.datetime.now().strftime("%I:%M %p")
+
+user_avatar = img_to_base64("./images/user.jpeg") 
+bot_avatar = img_to_base64("./images/bot.jpeg")     
+
+user_template = f'''
+<div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="data:image/png;base64,{user_avatar}">
     </div>
-    <div class="message">{{MSG}}</div>
+    <div class="message">
+        <div class="content">{{{{MSG}}}}</div>
+        <div class="timestamp">{get_timestamp()}</div>
+    </div>
 </div>
 '''
 
-user_template = '''
-<div class="chat-message user">
+bot_template = f'''
+<div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
-    </div>    
-    <div class="message">{{MSG}}</div>
+        <img src="data:image/png;base64,{bot_avatar}">
+    </div>
+    <div class="message">
+        <div class="content">{{{{MSG}}}}</div>
+        <div class="timestamp">{get_timestamp()}</div>
+    </div>
 </div>
 '''
